6ccf797ad867b21a0041718c24d096a657d74e0f
roycejwilliams <roycewilliamsj@gmail.com>
2023-12-04 13:13:08 -0600

    finsihing up footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <div className="w-full h-500 flex flex-col justify-center items-center bg-royal">
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-royal">
       {/* links */}
       <div className="w-5/6 h-450 flex items-center justify-center">
         <div className="w-1/2 h-full  flex justify-center items-center">
